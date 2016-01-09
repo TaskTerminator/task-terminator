576ad5f1505a7d7571ba56dc6c317bcd3bd7a781
hanthom <m.hantho@gmail.com>
2016-01-08 20:43:31 -0600

    Separated the models into their own files

--- server_assets/models/Position.js
+++ server_assets/models/Position.js
@@ -1,1 +1,11 @@
-Position.js
+var mongoose = require("mongoose");
+
+// Position/role employees have at the company.
+var positionSchema = mongoose.Schema({
+  name: {type:String, required:true},
+  employees: [
+        {type:String, ref:'Employee'}
+      ]
+}));
+
+module.exports = mongoose.model('Position', positionSchema);
--- server_assets/models/ProjectTask.js
+++ server_assets/models/ProjectTask.js
@@ -1,1 +1,20 @@
-ProjectTask.js
+var mongoose = require('mongoose');
+
+//Model of tasks tied to active projects. References department, position, and individual
+var projectTaskSchema = mongoose.Schema({
+  name: {type: String, required: true},
+  description: {type:String},
+  status: {type: String, enum: ['Complete', 'Incomplete'], default: 'Incomplete'},
+  associatedProject : {type:String, ref:'Project'},
+  date: {
+    created: {type: Date, default: new Date()},
+    deadline: {},
+  },
+  assigment: {
+      departments: {type:String, ref: 'Department'},
+      positions: {type:String, ref: 'Position'},
+      employees: {type:String, ref: 'Employee'}
+  }
+}));
+
+module.exports = mongoose.model('ProjectTask', projectTaskSchema);
