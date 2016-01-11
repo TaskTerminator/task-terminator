56cbaff1cdbabd2cb2c31ea22fc85dec89dfcf3c
glathrop <glathrop@gmail.com>
2016-01-11 15:30:50 -0600

    Employee CRUD Complete

--- src/server_assets/controllers/employeeCtrl.js
+++ src/server_assets/controllers/employeeCtrl.js
@@ -3,38 +3,55 @@ const Employee = require('../models/Employee.js');
 
 module.exports = {
 
-  newEmployee: function(req,res){
+  newEmployee: function(req, res) {
     console.log("POST - ADD EMPLOYEE ENDPOINT", req.body);
-    var employee = new Employee(req.body);
-    employee.identification.name.fullName = req.body.identification.name.firstName + " " + req.body.identification.name.lastName;
-    employee.save().then(function(err,result){
-        if(!err){
-          console.log("POST - ADD EMPLOYEE ERROR", err);
-        }
-        console.log("POST - ADD EMPLOYEE SUCCESS");
-      }
-    );
-    return res.status(201).end();
+    const newEmployee = new Employee(req.body);
+    newEmployee.identification.name.fullName = req.body.identification.name.firstName + " " + req.body.identification.name.lastName;
+    newEmployee.save().then((result) => {
+      return res.json(result);
+    }).catch((err) => {
+      return res.status(500).end();
+    });
   },
 
-  oneEmployee: function(req,res){
+  oneEmployee(req, res) {
     console.log("GET - EMPLOYEE ID: ", req.params.id);
-    return res.status(200).end();
+    Employee.findById(req.params.id).exec().then((employee) => {
+      return res.json(employee);
+    }).catch((err) => {
+      return res.status(500).end();
+    });
   },
 
-  deleteEmployee: function(req,res){
-    console.log("DELETE - EMPLOYEE ID: ", req.params.id);
-    return res.status(200).end();
-  },
-
-  editEmployee: function(req,res){
+  editEmployee(req, res) {
     console.log("EDIT - EMPLOYEE ID: ", req.params.id);
-    return res.status(200).end();
+    Employee.update({
+      _id: req.params.id
+    }, req.body).then(() => {
+      return res.status(200).end();
+    }).catch((err) => {
+      return res.status(500).end();
+    });
   },
 
-  allEmployees : function(req,res){
+  deleteEmployee(req, res) {
+    console.log("DELETE - EMPLOYEE ID: ", req.params.id);
+    Employee.remove({
+      _id: req.params.id
+    }, req.body).then(() => {
+      return res.status(200).end();
+    }).catch((err) => {
+      return res.status(500).end();
+    });
+  },
+
+  allEmployees(req, res) {
     console.log('GET - ALL EMPLOYEES ENDPOINT');
-    return res.status(200).end();
+    Employee.find().exec().then((positions) => {
+      return res.json(positions);
+    }).catch((err) => {
+      return res.status(500).end();
+    });
   }
 
 };
