b9dd744ad9ed4bc04b4489dc2750bc0f42050fa2
hanthom <m.hantho@gmail.com>
2016-01-11 15:39:33 -0600

    Updated all Department controller functions

--- src/server_assets/controllers/departmentCtrl.js
+++ src/server_assets/controllers/departmentCtrl.js
@@ -4,28 +4,50 @@ const Department = require('../models/Department.js');
 module.exports = {
 
   newDepartment: function(req, res) {
-    console.log("POST - ADD DEPARTMENT ENDPOINT");
-    return res.status(200).end();
+    console.log("POST - ADD DEPARTMENT ENDPOINT", req.body);
+    const newDepartment = new Department(req.body);
+    newDepartment.save().then((result) => {
+      return res.json(result);
+    }).catch((err) => {
+      return res.status(500).end();
+    });
   },
 
   oneDepartment: function(req,res){
     console.log("GET - DEPARTMENT ID: ", req.params.id);
-    return res.status(200).end();
+    Department.findById(req.params.id).exec().then((department) => {
+      return res.json(department);
+    }).catch((err) => {
+      return res.status(500).end();
+    });
   },
 
   editDepartment: function(req,res){
     console.log("PUT - EDIT DEPARTMENT ID: ", req.params.id);
-    return res.status(200).end();
+    Department.update({_id: req.params.id}, req.body).then(() => {
+      return res.status(200).end();
+    }).catch((err) => {
+      return res.status(500).end();
+    });
   },
 
   deleteDepartment: function(req,res){
     console.log('DELETE - DEPARTMENT ID: ', req.params.id);
-    return res.status(200).end();
+    Department.remove({_id: req.params.id}, req.body).then(() => {
+      return res.status(200).end();
+    }).catch((err) => {
+      return res.status(500).end();
+    });
   },
 
   allDepartments: function(req, res) {
     console.log('GET - ALL DEPARTMENTS ENDPOINT');
-    return res.status(200).end();
+    Department.find().exec().then((departments) => {
+      return res.json(departments);
+    }).catch((err) => {
+      console.log(err);
+      return res.status(500).end();
+    });
   }
 
 };
