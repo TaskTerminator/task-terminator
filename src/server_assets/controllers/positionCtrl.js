66444fe521ca7d025dfe1dcd2621148e5359c62f
SJTreadway <treadway08@gmail.com>
2016-01-11 13:08:00 -0600

    functional position controller

--- src/server_assets/controllers/positionCtrl.js
+++ src/server_assets/controllers/positionCtrl.js
@@ -3,33 +3,45 @@ const Position = require('../models/Position.js');
 
 module.exports = {
 
-  newPosition: function(req, res) {
-    console.log("POST - ADD POSITION ENDPOINT");
-    return res.status(200).end();
+  newPosition(req, res) {
+    const newPosition = new Position(req.body);
+    newPosition.save().then((result) => {
+      return res.json(result);
+    }).catch((err) => {
+      return res.status(500).end();
+    });
   },
 
-  onePosition: function(req,res){
-    console.log('GET - POSITION ID: ', req.params.id);
-    return res.status(200).end();
+  onePosition(req,res){
+    Position.findById(req.params.id).exec().then((position) => {
+      return res.json(position);
+    }).catch((err) => {
+      return res.status(500).end();
+    });
   },
 
-  editPosition: function(req,res){
-    console.log('EDIT - POSITION ID: ', req.params.id);
-    return res.status(200).end();
+  editPosition(req,res){
+    Position.update({_id: req.params.id}, req.body).then(() => {
+      return res.status(200).end();
+    }).catch((err) => {
+      return res.status(500).end();
+    });
   },
 
-  deletePosition: function(req,res){
-    console.log('DELETE - POSITION ID: ', req.params.id);
-    return res.status(200).end();
+  deletePosition(req,res){
+    Position.remove({_id: req.params.id}, req.body).then(() => {
+      return res.status(200).end();
+    }).catch((err) => {
+      return res.status(500).end();
+    });
   },
 
-  allPositions: function(req,res){
-    console.log("GET - ALL POSITIONS ENDPOINT");
-    return res.status(200).end();
+  allPositions(req,res){
+    Position.find().exec().then((positions) => {
+      return res.json(positions);
+    }).catch((err) => {
+      return res.status(500).end();
+    });
   }
-
-
-
-
 
 };
