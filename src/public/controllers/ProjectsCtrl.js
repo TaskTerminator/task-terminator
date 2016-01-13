c64d425668cdcffc4dd50a6da1a17d333d8751e9
Thomas Adair <thomascadair@gmail.com>
2016-01-13 13:32:54 -0600

    Modal in projects

--- src/public/controllers/ProjectsCtrl.js
+++ src/public/controllers/ProjectsCtrl.js
@@ -1,5 +1,12 @@
-angular.module('terminatorApp').controller('ProjectsCtrl', function($scope) {
+angular.module('terminatorApp').controller('ProjectsCtrl', function($scope, $uibModal) {
 
   $scope.cssClass = 'page-projects';
+
+  $scope.openModal = function() {
+  	var modalInstance = $uibModal.open({
+  		animation: true,
+  		templateUrl: "myModalContent.jade",
+  	})
+  }
 
 });
