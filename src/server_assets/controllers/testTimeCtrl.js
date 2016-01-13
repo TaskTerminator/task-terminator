fc44fffc37d83a77a9a444b71af431e60f95d8e0
glathrop <glathrop@gmail.com>
2016-01-13 15:21:50 -0600

    Refactoring for First Instance Issues

--- src/server_assets/controllers/testTimeCtrl.js
+++ src/server_assets/controllers/testTimeCtrl.js
@@ -21,12 +21,31 @@ module.exports = {
       console.log("BIWEEKLY SPECIFIC DAY - WED: ", time.biWeeklySpecificDay(3)._d);
       console.log("BIWEEKLY SPECIFIC DAY - FRI: ", time.biWeeklySpecificDay(5)._d);
       console.log("MONTHLY FIRST DAY: ", time.monthlyFirstDay()._d);
-      console.log("MONTHLY LAST DAY: ", time.monthlyLastDay()._d);
+      console.log(" ");
+      console.log("FIRST MONTHLY LAST DAY: ", time.monthlyLastDay("first")._d);
+      console.log("NEXT  MONTHLY LAST DAY: ", time.monthlyLastDay()._d);
+      console.log(" ");
+
       console.log("MONTHLY ANY DAY", time.monthlyAnyDay()._d);
       //SELECTION SHOULD BE LIMITED TO NO MORE THAN 28 ON FRONT END
-      console.log("MONTHLY 5 DAYS FROM START :",time.monthlyDaysFromStart(5)._d);
-      console.log("MONTHLY 13 DAYS FROM START :",time.monthlyDaysFromStart(13)._d);
-      console.log("MONTHLY 26 DAYS FROM START :",time.monthlyDaysFromStart(26)._d);
+      console.log(" ");
+      console.log("MONTHLY DAYS FROM START - FIRST INSTANCE - 5 DAYS  ", time.monthlyDaysFromStart(5,'first')._d);
+      console.log("MONTHLY DAYS FROM START - FIRST INSTANCE - 22 DAYS ",time.monthlyDaysFromStart(22,'first')._d);
+      console.log("NEXT INSTANCE MONTHLY 5 DAYS FROM START :",time.monthlyDaysFromStart(5)._d);
+      console.log(" ");
+
+      console.log(" ");
+      console.log("QUARTER DAYS FROM START - FIRST INSTANCE - 5 DAYS  ", time.quarterlyDaysFromStart(5,'first')._d);
+      console.log("QUARTER DAYS FROM START - FIRST INSTANCE - 45 DAYS ",time.quarterlyDaysFromStart(45,'first')._d);
+      console.log("NEXT INSTANCE QUARTER 5 DAYS FROM START :",time.quarterlyDaysFromStart(5)._d);
+      console.log(" ");
+
+      console.log(" ");
+      console.log("ANNUAL DAYS FROM START - FIRST INSTANCE - 5 DAYS  ", time.annuallyDaysFromStart(5,'first')._d);
+      console.log("ANNUAL DAYS FROM START - FIRST INSTANCE - 245 DAYS ",time.annuallyDaysFromStart(245,'first')._d);
+      console.log("NEXT INSTANCE ANNUAL 5 DAYS FROM START :",time.annuallyDaysFromStart(5)._d);
+      console.log(" ");
+
       console.log("MONTHLY 2 DAYS BEFORE END :",time.monthlyDaysBeforeEnd(2)._d);
       console.log("MONTHLY 9 DAYS BEFORE END :",time.monthlyDaysBeforeEnd(9)._d);
       console.log("MONTHLY 17 DAYS BEFORE END :",time.monthlyDaysBeforeEnd(17)._d);
@@ -35,14 +54,18 @@ module.exports = {
       console.log("QUARTER FIRST DAY: ", time.quarterlyFirstDay()._d);
       console.log("QUARTER LAST DAY: ", time.quarterlyLastDay()._d);
       console.log("QUARTER ANY DAY", time.quarterlyAnyDay()._d);
-      console.log("QUARTER 5 DAYS FROM START :",time.quarterlyDaysFromStart(5)._d);
-      console.log("QUARTER 13 DAYS FROM START :",time.quarterlyDaysFromStart(13)._d);
-      console.log("QUARTER 26 DAYS FROM START :",time.quarterlyDaysFromStart(26)._d);
+
+
+
       console.log("QUARTER 2 DAYS BEFORE END :",time.quarterlyDaysBeforeEnd(2)._d);
       console.log("QUARTER 9 DAYS BEFORE END :",time.quarterlyDaysBeforeEnd(9)._d);
-      console.log("QUARTER 17 DAYS BEFORE END :",time.quarterlyDaysBeforeEnd(17)._d);
+
       console.log("ANNUALLY FIRST DAY: ", time.annuallyFirstDay()._d);
-      console.log("ANNUALLY LAST DAY: ", time.annuallyLastDay()._d);
+      console.log(" ");
+      console.log("FIRST ANNUALLY LAST DAY: ", time.annuallyLastDay("first")._d);
+      console.log("NEXT  ANNUALLY LAST DAY: ", time.annuallyLastDay()._d);
+      console.log(" ");
+
       console.log("ANNUALLY ANY DAY", time.annuallyAnyDay()._d);
       console.log("ANNUALLY 5 DAYS FROM START :",time.annuallyDaysFromStart(5)._d);
       console.log("ANNUALLY 13 DAYS FROM START :",time.annuallyDaysFromStart(13)._d);
@@ -50,6 +73,14 @@ module.exports = {
       console.log("ANNUALLY 2 DAYS BEFORE END :",time.annuallyDaysBeforeEnd(2)._d);
       console.log("ANNUALLY 9 DAYS BEFORE END :",time.annuallyDaysBeforeEnd(9)._d);
       console.log("ANNUALLY 17 DAYS BEFORE END :",time.annuallyDaysBeforeEnd(17)._d);
+      console.log(" ");
+      console.log("ANNUALLY PARTICULAR MONTH - NATURAL INSTANCE JANUARY : ", time.annuallyParticularMonth(0, "first")._d);
+      console.log("ANNUALLY PARTICULAR MONTH - NATURAL INSTANCE JUNE : ", time.annuallyParticularMonth(5, "first")._d);
+      console.log("ANNUALLY PARTICULAR MONTH - JUNE : ", time.annuallyParticularMonth(5)._d);
+      console.log(" ");
+      console.log("FIRST INSTANCE ANNUALLY PARTICULAR QUARTER - 3rd : ", time.annuallyParticularQuarter(3,"first")._d);
+      console.log("NEXT INSTANCE  ANNUALLY PARTICULAR QUARTER - 3rd : ", time.annuallyParticularQuarter(3)._d);
+
 
 
       return res.status(200).end();
