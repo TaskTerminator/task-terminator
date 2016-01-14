670aa160943131501e37c55ad618cc6d4c7441b1
Thomas Adair <thomascadair@gmail.com>
2016-01-14 10:01:21 -0600

    messing with forms

--- src/public/controllers/ProjectsCtrl.js
+++ src/public/controllers/ProjectsCtrl.js
@@ -1,12 +1,17 @@
 angular.module('terminatorApp').controller('ProjectsCtrl', function($scope, $uibModal) {
 
   $scope.cssClass = 'page-projects';
+  $scope.yes = !$scope.yes;
 
   $scope.openModal = function() {
   	var modalInstance = $uibModal.open({
   		animation: true,
   		templateUrl: "myModalContent.jade",
   	})
-  }
+  };
+
+  $scope.change = function() {
+  	$scope.yes = !$scope.yes;
+  };
 
 });
