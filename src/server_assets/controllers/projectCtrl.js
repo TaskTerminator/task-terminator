ae597ab2a17521b39b1967d30b53f0c7e25a8f19
glathrop <glathrop@gmail.com>
2016-01-15 14:11:15 -0600

    Starting to Implement Promises

--- src/server_assets/controllers/projectCtrl.js
+++ src/server_assets/controllers/projectCtrl.js
@@ -10,57 +10,95 @@ const helpers = require('../controllers/projectHelpers');
 module.exports = {
 
   newProject(req, res) {
-      //Look up the template that is creating this task
-    Template.findById(req.params.templateid).exec().then((template) => {
-      //When the template is returned convert it to a true object rather than a mongoose object
-      var template_plain = template.toObject();
-      var origTemplate_tasks = template_plain.tasks;
-      console.log("ORIGINAL TEMPLATE TASKS",origTemplate_tasks);
+    var templateId = req.params.templateid;
+    var instance = req.body.instance;
 
-      //delete the ._id and version fields from the object
-      delete template_plain._id;
-      delete template_plain.__v;
-      delete template_plain.tasks;
+    helpers.makeProjectObject(templateId)
+      .then((cleanObject) => {
+        var newProject = new Project(cleanObject);
+        var associatedProjectId = newProject._id;
+        return helpers.nextOccurrence(cleanObject, instance);
+      })
+      .then((deadline) => {
+        newProject.setup.dueDate.actual = deadline;
+        return helpers.getTaskArray(templateid);
+      })
+      .then((arrayofIds) => {
+        //for every id, make a new project task
+        var new_tasks_promises = _.map(arrayofIds, function(id) {
+          return helpers.makeTemplateTaskObject(id, associatedProjectId);
+        });
+        //new_tasks_promises now contains an array of promises which will be resolved as each of the tasks are created
+        return q.all(new_tasks_promises);
+      })
+      .then((clean_objects) => {
 
-      //enstanciate a new project object
-      let newProject = new Project(template_plain);
-        //get the project specific description from the req.body
-        newProject.description = req.body.description;
+        return q.all(_.map(clean_objects, ((clean_object) => {
+          return helpers.makeProjectTask(clean_object);
+        })
+      }))
+      .spread((project_tasks) => {
+        //take tasks, put in Project, save
+      })
 
 
-        var newTaskIds = [];
-        for(var i = 0; i < origTemplate_tasks.length; i ++){
-          console.log("A Task",origTemplate_tasks[i]);
 
-          TemplateTask.findById(origTemplate_tasks[i])
-            .exec()
-            .then((returnedTask) => {
-              console.log("ORIGINAL TASK PRE MANIPULATION", returnedTask);
-              var task_plain = returnedTask.toObject();
-              delete task_plain._id;
-              delete task_plain.__v;
-              delete task_plain.date.created;
-              task_plain.associatedProject = newProject._id;
 
-              console.log("MANIPULATED TASK", task_plain);
-              var newProjectTask = new ProjectTask(task_plain);
-              console.log("NEW TASK ID", newProjectTask._id);
-              newTaskIds.push(newProjectTask._id);
-              newProjectTask.save();
-            });
-        }
 
-        newProject.tasks = newTaskIds;
 
-        newProject.save().then(() => {
-            return res.status(200).end();
-        }).catch((err) => {
-          console.log("bad dog", err);
-            return res.status(500).end();
-    }).catch((err) => {
-      console.log("badder dog", err);
-    });
-  });
+
+
+  //     //Look up the template that is creating this task
+  //   Template.findById(req.params.templateid).exec().then((template) => {
+  //     //When the template is returned convert it to a true object rather than a mongoose object
+  //     var template_plain = template.toObject();
+  //     var origTemplate_tasks = template_plain.tasks;
+  //     console.log("ORIGINAL TEMPLATE TASKS",origTemplate_tasks);
+  //
+  //     //delete the ._id and version fields from the object
+  //     delete template_plain._id;
+  //     delete template_plain.__v;
+  //     delete template_plain.tasks;
+  //
+  //     //enstanciate a new project object
+  //     let newProject = new Project(template_plain);
+  //       //get the project specific description from the req.body
+  //       newProject.description = req.body.description;
+  //
+  //
+  //       var newTaskIds = [];
+  //       for(var i = 0; i < origTemplate_tasks.length; i ++){
+  //         console.log("A Task",origTemplate_tasks[i]);
+  //
+  //         TemplateTask.findById(origTemplate_tasks[i])
+  //           .exec()
+  //           .then((returnedTask) => {
+  //             console.log("ORIGINAL TASK PRE MANIPULATION", returnedTask);
+  //             var task_plain = returnedTask.toObject();
+  //             delete task_plain._id;
+  //             delete task_plain.__v;
+  //             delete task_plain.date.created;
+  //             task_plain.associatedProject = newProject._id;
+  //
+  //             console.log("MANIPULATED TASK", task_plain);
+  //             var newProjectTask = new ProjectTask(task_plain);
+  //             console.log("NEW TASK ID", newProjectTask._id);
+  //             newTaskIds.push(newProjectTask._id);
+  //             newProjectTask.save();
+  //           });
+  //       }
+  //
+  //       newProject.tasks = newTaskIds;
+  //
+  //       newProject.save().then(() => {
+  //           return res.status(200).end();
+  //       }).catch((err) => {
+  //         console.log("bad dog", err);
+  //           return res.status(500).end();
+  //   }).catch((err) => {
+  //     console.log("badder dog", err);
+  //   });
+  // });
 },
 
   oneProject(req, res) {
@@ -97,5 +135,3 @@ module.exports = {
   }
 
 };
-
-    
--- src/server_assets/controllers/projectHelpers.js
+++ src/server_assets/controllers/projectHelpers.js
@@ -1,115 +1,117 @@
 const mongoose = require('mongoose');
 const Employee = require('../models/Employee.js');
 const Template = require('../models/Template.js');
+const Q = require('q');
 
 module.exports = {
 
-    getTaskArray(id) {
-        Template.findById(id)
-            .exec()
-            .then((template) => {
-                const taskArray = template.tasks;
-                return taskArray;
-            });
-    },
-    
-    makeTemplateTaskObject(id) {
-        TemplateTask.findById(id)
-            .exec()
-            .then((returnedTask) => {
-                console.log("ORIGINAL TASK PRE MANIPULATION", returnedTask);
-                const task_plain = returnedTask.toObject();
-                delete task_plain._id;
-                delete task_plain.__v;
-                delete task_plain.date.created;
-                return task_plain;
-            });
+  getTaskArray(id) {
+    var deferred = Q.defer();
+      Template.findById(id)
+        .exec()
+        .then((template) => {
+          const taskArray = template.tasks;
+          deferred.resolve(taskArray);
+        });
+      return deferred.promise;
     },
 
-    makeProjectObject(id){
-        Template.findById(id)
-            .exec()
-            .then((template) =>{
-                var new_Object = template.toObject();
-                delete new_Object._id;
-                delete new_Object.__v;
-                return new_Object;
-            });
+    makeTemplateTaskObject(id, associatedProjectId) {
+      var deferred = Q.defer();
+      TemplateTask.findById(id)
+        .exec()
+        .then((returnedTask) => {
+          console.log("ORIGINAL TASK PRE MANIPULATION", returnedTask);
+          const task_plain = returnedTask.toObject();
+          delete task_plain._id;
+          delete task_plain.__v;
+          delete task_plain.date.created;
+          deferred.resolve(task_plain);
+        });
+      return deferred.promise;
     },
-    
-    scheduledProject(instance) {
-        let deadline = '';
-        if (template_plain.setup.interval.type === 'Weekly') {
+
+    makeProjectObject(id) {
+      var deferred = Q.defer();
+      Template.findById(id)
+        .exec()
+        .then((template) => {
+          var new_Object = template.toObject();
+          delete new_Object._id;
+          delete new_Object.__v;
+          deferred.resolve(new_Object);
+        });
+      return deferred.promise;
+    },
+
+    nextOccurrence(template_plain, instance) {
+      var deferred = Q.defer();
+      let deadline = '';
+      if (template_plain.setup.interval.type === 'Weekly') {
         if (template_plain.setup.interval.weeklyInterval !== 'Any') {
-            deadline = timeCtrl.weeklySpecificDay(template_plain.setup.interval.weeklyInterval, instance);
-    } else {
-        deadline = timeCtrl.weeklyAnyDay(instance);
+          deadline = timeCtrl.weeklySpecificDay(template_plain.setup.interval.weeklyInterval, instance);
+        } else {
+          deadline = timeCtrl.weeklyAnyDay(instance);
         }
-    }
-    else if (template_plain.setup.interval.type === 'Bi-Weekly') {
+      } else if (template_plain.setup.interval.type === 'Bi-Weekly') {
         if (template_plain.setup.interval.weeklyInterval !== 'Any') {
-            deadline = timeCtrl.biWeeklySpecificDay(template_plain.setup.interval.biWeeklyInterval, instance);
-    } else {
-        deadline = timeCtrl.biWeeklyAnyDay(instance);
+          deadline = timeCtrl.biWeeklySpecificDay(template_plain.setup.interval.biWeeklyInterval, instance);
+        } else {
+          deadline = timeCtrl.biWeeklyAnyDay(instance);
         }
-    }
-    else if (template_plain.setup.interval.type === 'Monthly') {
+      } else if (template_plain.setup.interval.type === 'Monthly') {
         if (template_plain.setup.interval.monthlyInterval.firstOfMonth === true) {
-            deadline = timeCtrl.monthlyFirstDay();
-    } else if (template_plain.setup.interval.monthlyInterval.lastOfMonth === true) {
-        deadline = timeCtrl.monthlyLastDay(instance);
-    } else {
-        deadline = timeCtrl.monthlyAnyDay(instance);
+          deadline = timeCtrl.monthlyFirstDay();
+        } else if (template_plain.setup.interval.monthlyInterval.lastOfMonth === true) {
+          deadline = timeCtrl.monthlyLastDay(instance);
+        } else {
+          deadline = timeCtrl.monthlyAnyDay(instance);
         }
-    }
-    else if (template_plain.setup.interval.type === 'Semi-Monthly') {
+      } else if (template_plain.setup.interval.type === 'Semi-Monthly') {
         deadline = timeCtrl.semiMonthlyFirstCycle(template_plain.setup.interval.semiMonthlyInterval.fromBeginning);
         template_plain.setup.dueDate.anticipated = timeCtrl.semiMonthlySecondCycle(template_plain.setup.interval.semiMonthlyInterval.fromEnd);
-    }
-    else if (template_plain.setup.interval.type === 'Quarterly') {
+      } else if (template_plain.setup.interval.type === 'Quarterly') {
         if (template_plain.setup.interval.quarterlyInterval.selection === 'First Day of the Quarter') {
-            deadline = timeCtrl.quarterlyFirstDay();
+          deadline = timeCtrl.quarterlyFirstDay();
         } else if (template_plain.setup.interval.quarterlyInterval.selection === 'Last Day of the Quarter') {
-            deadline = timeCtrl.quarterlyLastDay(instance);
+          deadline = timeCtrl.quarterlyLastDay(instance);
         } else if (template_plain.setup.interval.quarterlyInterval.selection === '# Days from Start') {
-            deadline = timeCtrl.quarterlyDaysFromStart(template_plain.setup.interval.quarterlyInterval.fromBeginning, instance);
+          deadline = timeCtrl.quarterlyDaysFromStart(template_plain.setup.interval.quarterlyInterval.fromBeginning, instance);
         } else if (template_plain.setup.interval.quarterlyInterval.selection === '# Days from End') {
-            deadline = timeCtrl.quarterlyDaysBeforeEnd(template_plain.setup.interval.quarterlyInterval.fromEnd, instance);
+          deadline = timeCtrl.quarterlyDaysBeforeEnd(template_plain.setup.interval.quarterlyInterval.fromEnd, instance);
         } else {
-            deadline = timeCtrl.quarterlyAnyDay(instance);
+          deadline = timeCtrl.quarterlyAnyDay(instance);
         }
-    } 
-    else if (template_plain.setup.interval.type === 'Annually') {
+      } else if (template_plain.setup.interval.type === 'Annually') {
         if (template_plain.setup.interval.annualInterval === 'First Day of the Year') {
-            deadline = timeCtrl.annuallyFirstDay(instance);
+          deadline = timeCtrl.annuallyFirstDay(instance);
         } else if (template_plain.setup.interval.annualInterval.selection === 'Last Day of the Year') {
-            deadline = timeCtrl.annuallyLastDay(instance);
+          deadline = timeCtrl.annuallyLastDay(instance);
         } else if (template_plain.setup.interval.annualInterval.selection === 'In a Particular Month') {
-            deadline = timeCtrl.annuallyParticularMonth(template_plain.setup.interval.annualInterval.selectMonth, instance);
+          deadline = timeCtrl.annuallyParticularMonth(template_plain.setup.interval.annualInterval.selectMonth, instance);
         } else if (template_plain.setup.interval.annualInterval.selection === 'In a Particular Quarter') {
-            deadline = timeCtrl.annuallyParticularQuarter(template_plain.setup.interval.annualInterval.selectQuarter, instance);
+          deadline = timeCtrl.annuallyParticularQuarter(template_plain.setup.interval.annualInterval.selectQuarter, instance);
         } else if (template_plain.setup.interval.annualInterval.selection === '# of Days From Start') {
-            deadline = timeCtrl.annuallyDaysFromStart(template_plain.setup.interval.annualInterval.fromBeginning, instance);
+          deadline = timeCtrl.annuallyDaysFromStart(template_plain.setup.interval.annualInterval.fromBeginning, instance);
         } else if (template_plain.setup.interval.annualInterval.selection === '# of Days Before End') {
-            deadline = timeCtrl.annuallyDaysBeforeEnd(template_plain.setup.interval.annualInterval.fromEnd, instance);
+          deadline = timeCtrl.annuallyDaysBeforeEnd(template_plain.setup.interval.annualInterval.fromEnd, instance);
         } else {
-            deadline = timeCtrl.annuallyAnyDay(instance);
+          deadline = timeCtrl.annuallyAnyDay(instance);
         }
-    };
-        deadline = template_plain.setup.dueDate.actual;
-        return deadline;
+      }
+      deferred.resolve(deadline);
+      return deferred.promise;
     },
-    
+
     triggeredProject(date) {
-        let deadline = '';
-        if (template_plain.setup.dueDate.target === 'Today') {
-            date = timeCtrl.deadlineToday();
-        }
-        else if (template_plain.setup.dueDate.target === 'Specific Date') {
-            date = timeCtrl.dateDeadline(date);
-        }
-        deadline = template_plain.setup.dueDate.actual;
-        return deadline;
+      let deadline = '';
+      if (template_plain.setup.dueDate.target === 'Today') {
+        date = timeCtrl.deadlineToday();
+      } else if (template_plain.setup.dueDate.target === 'Specific Date') {
+        date = timeCtrl.dateDeadline(date);
+      }
+      deadline = template_plain.setup.dueDate.actual;
+      return deadline;
     },
 
 };
