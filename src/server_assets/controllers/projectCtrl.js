c025fd5645df80d74538abd08867c56ad06d9bb6
SJTreadway <treadway08@gmail.com>
2016-01-15 11:36:06 -0600

    starts building project helper functions

--- src/server_assets/controllers/projectCtrl.js
+++ src/server_assets/controllers/projectCtrl.js
@@ -4,6 +4,7 @@ const ProjectTask = require('../models/ProjectTask.js');
 const Template = require('../models/Template');
 const TemplateTask = require('../models/TemplateTask.js');
 const timeCtrl = require('../controllers/timeCtrl');
+const helpers = require('../controllers/projectHelpers');
 
 
 module.exports = {
@@ -20,64 +21,6 @@ module.exports = {
       delete template_plain._id;
       delete template_plain.__v;
       delete template_plain.tasks;
-        
-    if (template_plain.setup.interval.type === 'Weekly') {
-        if (template_plain.setup.interval.weeklyInterval !== 'Any') {
-            template_plain.setup.dueDate.actual = timeCtrl.weeklySpecificDay(template_plain.setup.interval.weeklyInterval, req.body.instance);
-    } else {
-        template_plain.setup.dueDate.actual = timeCtrl.weeklyAnyDay(req.body.instance);
-        }
-    }
-    else if (template_plain.setup.interval.type === 'Bi-Weekly') {
-        if (template_plain.setup.interval.weeklyInterval !== 'Any') {
-            template_plain.setup.dueDate.actual = timeCtrl.biWeeklySpecificDay(template_plain.setup.interval.biWeeklyInterval, req.body.instance);
-    } else {
-        template_plain.setup.dueDate.actual = timeCtrl.biWeeklyAnyDay(req.body.instance);
-        }
-    }
-    else if (template_plain.setup.interval.type === 'Monthly') {
-        if (template_plain.setup.interval.monthlyInterval.firstOfMonth === true) {
-            template_plain.setup.dueDate.actual = timeCtrl.monthlyFirstDay();
-    } else if (template_plain.setup.interval.monthlyInterval.lastOfMonth === true) {
-        template_plain.setup.dueDate.actual = timeCtrl.monthlyLastDay(req.body.instance);
-    } else {
-        template_plain.setup.dueDate.actual = timeCtrl.monthlyAnyDay(req.body.instance);
-        }
-    }
-    else if (template_plain.setup.interval.type === 'Semi-Monthly') {
-        template_plain.setup.dueDate.actual = timeCtrl.semiMonthlyFirstCycle(template_plain.setup.interval.semiMonthlyInterval.fromBeginning);
-        template_plain.setup.dueDate.anticipated = timeCtrl.semiMonthlySecondCycle(template_plain.setup.interval.semiMonthlyInterval.fromEnd);
-    }
-    else if (template_plain.setup.interval.type === 'Quarterly') {
-        if (template_plain.setup.interval.quarterlyInterval.selection === 'First Day of the Quarter') {
-            template_plain.setup.dueDate.actual = timeCtrl.quarterlyFirstDay();
-        } else if (template_plain.setup.interval.quarterlyInterval.selection === 'Last Day of the Quarter') {
-            template_plain.setup.dueDate.actual = timeCtrl.quarterlyLastDay(req.body.instance);
-        } else if (template_plain.setup.interval.quarterlyInterval.selection === '# Days from Start') {
-            template_plain.setup.dueDate.actual = timeCtrl.quarterlyDaysFromStart(template_plain.setup.interval.quarterlyInterval.fromBeginning, req.body.instance);
-        } else if (template_plain.setup.interval.quarterlyInterval.selection === '# Days from End') {
-            template_plain.setup.dueDate.actual = timeCtrl.quarterlyDaysBeforeEnd(template_plain.setup.interval.quarterlyInterval.fromEnd, req.body.instance);
-        } else {
-            template_plain.setup.dueDate.actual = timeCtrl.quarterlyAnyDay(req.body.instance);
-        }
-    } 
-    else if (template_plain.setup.interval.type === 'Annually') {
-        if (template_plain.setup.interval.annualInterval === 'First Day of the Year') {
-            template_plain.setup.dueDate.actual = timeCtrl.annuallyFirstDay(req.body.instance);
-        } else if (template_plain.setup.interval.annualInterval.selection === 'Last Day of the Year') {
-            template_plain.setup.dueDate.actual = timeCtrl.annuallyLastDay(req.body.instance);
-        } else if (template_plain.setup.interval.annualInterval.selection === 'In a Particular Month') {
-            template_plain.setup.dueDate.actual = timeCtrl.annuallyParticularMonth(template_plain.setup.interval.annualInterval.selectMonth, req.body.instance);
-        } else if (template_plain.setup.interval.annualInterval.selection === 'In a Particular Quarter') {
-            template_plain.setup.dueDate.actual = timeCtrl.annuallyParticularQuarter(template_plain.setup.interval.annualInterval.selectQuarter, req.body.instance);
-        } else if (template_plain.setup.interval.annualInterval.selection === '# of Days From Start') {
-            template_plain.setup.dueDate.actual = timeCtrl.annuallyDaysFromStart(template_plain.setup.interval.annualInterval.fromBeginning, req.body.instance);
-        } else if (template_plain.setup.interval.annualInterval.selection === '# of Days Before End') {
-            template_plain.setup.dueDate.actual = timeCtrl.annuallyDaysBeforeEnd(template_plain.setup.interval.annualInterval.fromEnd, req.body.instance);
-        } else {
-            template_plain.setup.dueDate.actual = timeCtrl.annuallyAnyDay(req.body.instance);
-        }
-    };
 
       //enstanciate a new project object
       let newProject = new Project(template_plain);
