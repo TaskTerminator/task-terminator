f5e007ce9ad5e32e691f1c95c1f9d3253894cc9b
Thomas Adair <thomascadair@gmail.com>
2016-01-18 11:39:33 -0600

    attempted connect single project form

--- src/public/controllers/ProjectsCtrl.js
+++ src/public/controllers/ProjectsCtrl.js
@@ -68,9 +68,15 @@ angular.module('terminatorApp').controller('ProjectsCtrl', function($scope, $uib
     $scope.showTaskBox = !$scope.showTaskBox;
   };
 
-  $scope.postProject = function() {
-    var modalInstance = $uibModal.dismiss('cancel');
-  }
+  $scope.newSingleProject = {
+    setup: {type: "Single"}
+  };
+  $scope.postSingleProject = function(newSingleProject) {
+    console.log("New Single Project Info: ", newSingleProject);
+    ProjectsSvc.postProject(newSingleProject).then(function(results) {
+      console.log("Single Project added: ", results);
+    })
+  };
 //////////////////////////////////////////////////////////
 
 
--- src/public/services/ProjectsSvc.js
+++ src/public/services/ProjectsSvc.js
@@ -64,6 +64,22 @@ angular.module('terminatorApp').service('ProjectsSvc', function($http, $q) {
     return defer.promise;
   };
 
+  this.postSingleProject = function (newSingleProject) {
+    var defer = $q.defer();
+
+    $http({
+      method: "POST",
+      url: "/api/project",
+      data: newSingleProject
+    }).then(function(response) {
+      defer.resolve(response);
+    }, function (error) {
+      console.log("error: ", error);
+    });
+
+    return defer.promise;
+  };
+
   this.editProject = function () {
     var defer = $q.defer();
 
