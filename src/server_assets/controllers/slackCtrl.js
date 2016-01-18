671beda9aefc052b4c56c15648f40a1e0d5bd81e
Thomas Adair <thomascadair@gmail.com>
2016-01-18 15:51:48 -0600

    Got add emplyee form to populate departments and positions within a selector

--- src/server_assets/controllers/slackCtrl.js
+++ src/server_assets/controllers/slackCtrl.js
@@ -10,13 +10,13 @@ const controller = Botkit.slackbot({
 	});
 
 	// connect the bot to a stream of messages
-	controller.spawn({
-	  token: 'xoxb-18104911812-5iVtooMM6dfCQcfyb8W9Rwoc',
-	}).startRTM(function(err) {
-    if (err) {
-      throw new Error(err);
-    }
-  });
+	// controller.spawn({
+	//   token: 'xoxb-18104911812-5iVtooMM6dfCQcfyb8W9Rwoc',
+	// }).startRTM(function(err) {
+ //    if (err) {
+ //      throw new Error(err);
+ //    }
+ //  });
 
 	//LINK THE BOT TO WIT.AI
 	const witbot = Witbot("CZPINC6EVOQ7DCPZSUUBQ3B5GWQVOQ66"
