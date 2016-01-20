94142d36cd0a34d2d24a5d34b4d24962952f49cc
hanthom <m.hantho@gmail.com>
2016-01-20 15:58:52 -0600

    The rest of the template task post is working

--- src/server_assets/controllers/templateTaskCtrl.js
+++ src/server_assets/controllers/templateTaskCtrl.js
@@ -7,23 +7,26 @@ const time = require('../controllers/timeCtrl.js');
 module.exports = {
 
     addTask(req, res) {
-      const newTemplateTask = new TemplateTask(req.body);
-      Template
-        .findOne({
-          _id: req.params.templateid
-        })
-        .exec()
-        .then(function(result) {
-          // console.log(result);
-          result.tasks.push(newTemplateTask._id);
-          result.save();
-        });
+      console.log(req.body);
+      for(var i = 0; i < req.body.length; i++) {
+        const newTemplateTask = new TemplateTask(req.body[i]);
+        Template
+          .findOne({
+            _id: req.params.templateid
+          })
+          .exec()
+          .then(function(result) {
+            // console.log(result);
+            result.tasks.push(newTemplateTask._id);
+            result.save();
+          });
 
-        newTemplateTask.save().then((result) => {
-            return res.json(result);
-        }).catch((err) => {
-            return res.status(500).end();
-        });
+          newTemplateTask.save().then((result) => {
+              return res.json(result);
+          }).catch((err) => {
+              return res.status(500).end();
+          });
+        }
     },
 
     getAllTasks(req, res) {
