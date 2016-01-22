c7c4143de56b7a75a33244d042584c97e8b0a353
glathrop <glathrop@gmail.com>
2016-01-22 08:24:18 -0600

    Demo Ready

--- src/server_assets/controllers/slackCtrl.js
+++ src/server_assets/controllers/slackCtrl.js
@@ -11,13 +11,16 @@ const controller = Botkit.slackbot({
 	});
 
 	// connect the bot to a stream of messages
-//	controller.spawn({
-//	  token: 'xoxb-18104911812-lrix7VmoDeWSS4PTA8SxNFnN',
-//	}).startRTM(function(err) {
-//    if (err) {
-//      throw new Error(err);
-//    }
-//  });
+	controller.spawn({
+		//DevMtn Token
+	  // token: 'xoxb-18104911812-lrix7VmoDeWSS4PTA8SxNFnN',
+		//Our Slack Token
+		token: 'xoxb-19173759013-cGyV554J5XzYpOvptXNBWh53',
+	}).startRTM(function(err) {
+   if (err) {
+     throw new Error(err);
+   }
+ });
 
 	//LINK THE BOT TO WIT.AI
 	const witbot = Witbot("CZPINC6EVOQ7DCPZSUUBQ3B5GWQVOQ66"
