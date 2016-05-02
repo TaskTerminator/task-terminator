684472e799596f577ed6c20fd71cea57154de5a9
SJTreadway <treadway08@gmail.com>
2016-05-01 19:17:56 -0500

    test

--- src/server_assets/controllers/slackCtrl.js
+++ src/server_assets/controllers/slackCtrl.js
@@ -15,7 +15,7 @@ const controller = Botkit.slackbot({
 //	 	//DevMtn Token
 //	   // token: 'xoxb-18104911812-lrix7VmoDeWSS4PTA8SxNFnN',
 //	 	//Our Slack Token
-	 	token: 'xoxb-19170951524-61d475a76f9b19d9be7ee95519ab0fb8',
+	 	token: 'xoxp-19170951524-19175034981-39295117508-32bf38a3e3',
 	 }).startRTM((err) {
     if (err) {
       throw new Error(err);
